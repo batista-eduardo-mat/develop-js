7472b2b0585ca0c731244e264f9a3fbd8c8d8925
Eduardo Batista <batista.eduardo.mat@gmail.com>
2022-10-09 23:16:11 -0500

    es8 terminado es9

--- src/es8/03-string-padding.js
+++ src/es8/03-string-padding.js
@@ -1,5 +1,19 @@
 const string = 'Hello';
 // El primer parametro number, total de caracteres.
 // el segundo el caracter(es) parra rellenar y completar  
-console.log(string.padStart(10, '#'));
-console.log(string.padEnd(10, '#'));
+
+// console.log(string.padStart(10, '#'));
+// console.log(string.padEnd(10, '#'));
+let padding_start = 10;
+let padding_end = 10;
+// console.log(string.padStart(string.length + padding_start, '#').padEnd(string.length + padding_end + padding_start, '#'));
+
+function padding(string, ...values){
+    if (!values[0]) values[0]= 0;   
+    if (!values[1]) values[1]= '';   
+    if (!values[2]) values[2]= 0;   
+    if (!values[3]) values[3]= '';   
+    return string.padStart(string.length + parseInt(values[0]), `${values[1]}`).padEnd(string.length + parseInt(values[0])+ parseInt(values[2]),`${values[3]}`);
+}
+
+console.log(padding('Hola',5,'-',2,'__'));
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const arrayAges = [35, 60, 45, 27, 28, , , ,12];
+
+console.log(arrayAges);
+console.log(arrayAges.length);
